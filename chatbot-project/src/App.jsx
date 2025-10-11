6f30a3394407a9e1bb92ba2a26c12a4ca715c6ce
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-10-11 22:42:04 +0900

    space

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div>
 
         <a href="https://vite.dev" target="_blank">
-        
+
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -19,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
