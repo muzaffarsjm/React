ee7b30b383f00ecc6ed82e01e5475d029c5e9950
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-10-12 23:38:40 +0900

    space read

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -19,8 +19,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      
+
       <div className="card">
+        
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
